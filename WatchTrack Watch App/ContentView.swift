b9792199b4c2356bd311cba7bd143fa225cf6ecf
30109922 <30109922@students.southwales.ac.uk>
2026-02-22 08:17:06 +0000

    Initial Commit

---
+++ WatchTrack Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchTrack Watch App
+//
+//  Created by Junayed Al Aditto on 22/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchTrack Watch App/WatchTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTrackApp.swift
+//  WatchTrack Watch App
+//
+//  Created by Junayed Al Aditto on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WatchTrack_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
